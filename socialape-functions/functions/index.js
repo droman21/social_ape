7c5da8e394930744cdb927c62e2f901f9236ccbe
Daniel Roman <dan.roman21@gmail.com>
2020-08-23 16:19:20 -0400

    First pass at adding the email auth function

--- socialape-functions/functions/index.js
+++ socialape-functions/functions/index.js
@@ -1,39 +1,53 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
-
+const app = require('express')();
 admin.initializeApp();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello World!");
-});
+const firebaseConfig = {
+    apiKey: "AIzaSyD0XBN9fslCSB6CxJ1zjCFwggm5c0P9pMI",
+    authDomain: "socialape-a01e4.firebaseapp.com",
+    databaseURL: "https://socialape-a01e4.firebaseio.com",
+    projectId: "socialape-a01e4",
+    storageBucket: "socialape-a01e4.appspot.com",
+    messagingSenderId: "482186640561",
+    appId: "1:482186640561:web:2faf189242e458048d3690",
+    measurementId: "G-RJ14TF4NJ7"
+  };
 
-exports.getScreams = functions.https.onRequest((req, res) => {
+
+
+const firebase = require('firebase');
+firebase.initializeApp(firebaseConfig);
+
+
+app.get('/screams', (req, res) => {
     admin
-    .firestore()
-    .collection('screams')
-    .get()
-    .then((data) => {
-        let screams = [];
-        data.forEach((doc) => {
-            screams.push(doc.data());
+        .firestore()
+        .collection('screams')
+        .orderBy('createdAt', 'desc')
+        .get()
+        .then((data) => {
+            let screams = [];
+            data.forEach((doc) => {
+                screams.push({
+                    screamId: doc.id, 
+                    body: doc.data().body,
+                    userHandle: doc.data().userHandle,
+                    createdAt: doc.data().createdAt
+                });
         });
         return res.json(screams);
     })
     .catch((err) => console.error(err));
 });
 
-exports.createScream = functions.https.onRequest((req, res) => {
-    if (req.method !== 'POST') {
-        return res.status(400).json({ error: 'Method not allowed' });
-    }
+app.post('/scream', (req, res) => {
+
     const newScream = {
         body: req.body.body,
         userHandle: req.body.userHandle,
-        createdAt: admin.firestore.Timestamp.fromDate(new Date())
+        createdAt: new Date().toISOString()
     };
 
     admin
@@ -48,3 +62,32 @@ exports.createScream = functions.https.onRequest((req, res) => {
             console.error(err);
         });
 });
+
+// Signup Route
+app.post('/signup', (req, res) => {
+    const newUser = {
+        email: req.body.email,
+        password: req.body.password,
+        confirmPassword: req.body.confirmPassword,
+        handle: req.body.handle,
+    };
+
+
+//TODO Validate Data
+
+    firebase
+        .auth()
+        .createUserWithEmailAndPassword(newUser.email, newUser.password)
+        .then((data) => {
+            return res
+            .status(201)
+            .json({ message: `user ${data.user.uid} signed up successfully`});
+        })
+        .catch((err) => {
+            console.error(err);
+            return res.status(500).json({ error: err.code});
+        });
+});
+
+exports.api = functions.https.onRequest(app);
+
