6d8e88563e570baa8c22539a21c98d77fa92347f
Daniel Roman <dan.roman21@gmail.com>
2020-08-23 15:05:36 -0400

    Added client side 400 server error to index.js

--- socialape-functions/functions/index.js
+++ socialape-functions/functions/index.js
@@ -27,20 +27,24 @@ exports.getScreams = functions.https.onRequest((req, res) => {
 });
 
 exports.createScream = functions.https.onRequest((req, res) => {
+    if (req.method !== 'POST') {
+        return res.status(400).json({ error: 'Method not allowed' });
+    }
     const newScream = {
         body: req.body.body,
         userHandle: req.body.userHandle,
         createdAt: admin.firestore.Timestamp.fromDate(new Date())
     };
 
-    admin.firestore()
+    admin
+        .firestore()
         .collection('screams')
         .add(newScream)
-        .then(doc => {
-            res.json({ message: `document ${doc.id} created successfully`});
+        .then((doc) => {
+            res.json({ message: `document ${doc.id} created successfully` });
         })
-        .catch(err => {
-            res.status(500).json({ error: 'something went wrong :('});
+        .catch((err) => {
+            res.status(500).json({ error: 'something went wrong :(' });
             console.error(err);
-        })
+        });
 });
