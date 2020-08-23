7f60efa313db98b1432c183f4d6fd5d796962d41
Daniel Roman <dan.roman21@gmail.com>
2020-08-23 14:11:27 -0400

    Add screams to Firebase db, tested response in Postman, added new functions to index.js

--- socialape-functions/functions/index.js
+++ socialape-functions/functions/index.js
@@ -5,12 +5,42 @@ admin.initializeApp();
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
-//
+
 exports.helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info("Hello logs!", {structuredData: true});
   response.send("Hello World!");
 });
 
-exports.getScreams = functions.https.onRequest((req, response) => {
-    admin.firestore().collection('')
-})
+exports.getScreams = functions.https.onRequest((req, res) => {
+    admin
+    .firestore()
+    .collection('screams')
+    .get()
+    .then((data) => {
+        let screams = [];
+        data.forEach((doc) => {
+            screams.push(doc.data());
+        });
+        return res.json(screams);
+    })
+    .catch((err) => console.error(err));
+});
+
+exports.createScream = functions.https.onRequest((req, res) => {
+    const newScream = {
+        body: req.body.body,
+        userHandle: req.body.userHandle,
+        createdAt: admin.firestore.Timestamp.fromDate(new Date())
+    };
+
+    admin.firestore()
+        .collection('screams')
+        .add(newScream)
+        .then(doc => {
+            res.json({ message: `document ${doc.id} created successfully`});
+        })
+        .catch(err => {
+            res.status(500).json({ error: 'something went wrong :('});
+            console.error(err);
+        })
+});
