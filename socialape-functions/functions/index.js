7098fbd82c7c5356f815cfa71b668fd7f5600006
Daniel Roman <dan.roman21@gmail.com>
2020-08-23 18:21:30 -0400

    Updated wrong password functions

--- socialape-functions/functions/index.js
+++ socialape-functions/functions/index.js
@@ -99,6 +99,7 @@ app.post('/signup', (req, res) => {
 
     if (Object.keys(errors).length > 0) return res.status(400).json(errors);
 
+
 //TODO Validate Data
     let token, userId;
     db.doc(`/users/${newUser.handle}`)
@@ -136,6 +137,38 @@ app.post('/signup', (req, res) => {
             } else {
                 return res.status(500).json({ error: err.code});
             }
+        });
+});
+
+app.post('/login', (req, res) => {
+    const user = {
+        email: req.body.email,
+        password: req.body.password
+    };
+
+    let errors = {};
+
+    if (isEmpty(user.email)) errors.email = 'Must not be empty. Thx.';
+    if (isEmpty(user.password)) errors.password = 'Must not be empty. Thx.';
+
+    if(Object.keys(errors).length > 0) return res.status(400).json(errors);
+
+    firebase
+    .auth()
+    .signInWithEmailAndPassword(user.email, user.password)
+        .then((data) => {
+            return data.user.getIdToken();
+        })
+        .then((token) => {
+            return res.json({token});
+        })
+        .catch((err) => {
+            console.error(err);
+            if (err.code === 'auth/wrong-password'){
+                return res
+                    .status(403)
+                    .json({ general: 'Wrong credentials, please enter them again.'});
+            } else return res.status(500).json({error: err.code});
         });
 });
 
