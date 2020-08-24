b1af2480700533023a27415ad314e0919f2807b8
Daniel Roman <dan.roman21@gmail.com>
2020-08-24 11:26:00 -0400

    First commit

---
+++ functions/util/config.js
@@ -0,0 +1,21 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD0XBN9fslCSB6CxJ1zjCFwggm5c0P9pMI",
+    authDomain: "socialape-a01e4.firebaseapp.com",
+    databaseURL: "https://socialape-a01e4.firebaseio.com",
+    projectId: "socialape-a01e4",
+    storageBucket: "socialape-a01e4.appspot.com",
+    messagingSenderId: "482186640561",
+    appId: "1:482186640561:web:2faf189242e458048d3690",
+    measurementId: "G-RJ14TF4NJ7"
+};
+
+module.exports = {
+    apiKey: "AIzaSyD0XBN9fslCSB6CxJ1zjCFwggm5c0P9pMI",
+    authDomain: "socialape-a01e4.firebaseapp.com",
+    databaseURL: "https://socialape-a01e4.firebaseio.com",
+    projectId: "socialape-a01e4",
+    storageBucket: "socialape-a01e4.appspot.com",
+    messagingSenderId: "482186640561",
+    appId: "1:482186640561:web:2faf189242e458048d3690",
+    measurementId: "G-RJ14TF4NJ7"
+};
