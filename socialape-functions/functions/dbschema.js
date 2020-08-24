08ccfdaac08203ea056a50f60a3042148d20362f
Daniel Roman <dan.roman21@gmail.com>
2020-08-23 21:24:27 -0400

    Migration of files to master branch

---
+++ socialape-functions/functions/dbschema.js
@@ -0,0 +1,11 @@
+let db = {
+    screams: [
+        {
+            userHandle: 'user',
+            body: 'this is the scream body',
+            createdAt: '2020-08-23T19:52:55.339Z',
+            likeCount: 5,
+            commentCount: 2
+        }
+    ]
+}
--- socialape-functions/functions/index.js
+++ socialape-functions/functions/index.js
@@ -1,16 +1,23 @@
 const functions = require('firebase-functions');
-const admin = require('firebase-admin');
 
-admin.initializeApp();
+const app = require('express')();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello World!");
-});
+const FBAuth = require('./util/fbAuth')
 
-exports.getScreams = functions.https.onRequest((req, response) => {
-    admin.firestore().collection('')
-})
+const { getAllScreams } = require('./handlers/screams');
+const { signup, login } = require('./handlers/users');
+
+const firebase = require('firebase');
+firebase.initializeApp(firebaseConfig);
+
+//Scream Routes:
+app.get('/screams', getAllScreams);
+app.post('/scream', FBAuth, postOneScream);
+
+//User Routes:
+app.post('/signup', signup);
+app.post('/login', login);
+
+
+exports.api = functions.https.onRequest(app);
+
